5238fb16de997fba9dbe6de984cc456e46ddd7e8
Dmitri <dz@stackstorm.com>
2018-03-29 23:01:42 -0700

    Reset GGC with virgin group.json

--- scripts/update_ggc.sh
+++ scripts/update_ggc.sh
@@ -1,4 +1,13 @@
 
+set -x
+# Copy certificates
 sudo cp /vagrant/certs/* /greengrass/certs
 sudo cp /vagrant/config/* /greengrass/config
 sudo cp /vagrant/downloads/root.ca.pem /greengrass/certs
+
+# A previous group definition is no good no more, restore original
+sudo cp /greengrass/ggc/deployment/group/group.json /greengrass/ggc/deployment/group/group.bak
+sudo cp /greengrass/ggc/deployment/group/group.json.orig /greengrass/ggc/deployment/group/group.json
+set +x
+# Remind to restart GGC daemon
+echo "Restart GGC daemon to pick up the changes: greengrassd restart"
