5cbe2f0d82918d74c1c1a41c5ed4908c6f7807ae
Dmitri <dz@stackstorm.com>
2018-05-22 08:18:10 -0700

    Update greengrass core to 1.5

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y sqlite3 python2.7 binutils curl
 wget -O /vagrant/downloads/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
 
 # Copy greengrass binaries
-sudo tar -xzf /vagrant/downloads/greengrass-ubuntu-x86-64-1.3.0.tar.gz -C /
+sudo tar -xzf /vagrant/downloads/greengrass-ubuntu-x86-64-1.5.0.tar.gz -C /
 
 # Copy certificates and configurations
 sudo cp /vagrant/certs/* /greengrass/certs
