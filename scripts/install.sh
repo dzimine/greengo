f0debb2e45cf9574787d5a150819d3917e50792b
Dmitri Zimine <dz@stackstorm.com>
2018-12-12 13:55:25 -0800

    Download binaries instead of keeping them in repo.

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,19 @@ sudo groupadd --system ggc_group
 sudo apt-get update
 sudo apt-get install -y sqlite3 python2.7 binutils curl
 
-wget -O /vagrant/downloads/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
+# Download Greengrass binaries
+wget -P /vagrant/downloads https://d1onfpft10uf5o.cloudfront.net/greengrass-core/downloads/1.7.0/greengrass-linux-x86-64-1.7.0.tar.gz
 
 # Copy greengrass binaries
 sudo tar -xzf /vagrant/downloads/greengrass-linux-x86-64-1.7.0.tar.gz -C /
 
+# Download root certificate
+wget -O /vagrant/certs/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
+
 # Copy certificates and configurations
 sudo cp /vagrant/certs/* /greengrass/certs
 sudo cp /vagrant/config/* /greengrass/config
-sudo cp /vagrant/downloads/root.ca.pem /greengrass/certs
+sudo cp /vagrant/certs/root.ca.pem /greengrass/certs
 
 # Back up group.json - you'll thank me later
 sudo cp /greengrass/ggc/deployment/group/group.json /greengrass/ggc/deployment/group/group.json.orig
